5675bc6d1ec6792e45c4ac32280908ab97e2c6a8
Ramen Sayami <ramendra.manandhar@f1soft.com>
2020-05-24 16:49:15 +0545

    testing in progress

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,12 +1,12 @@
 class BoardsController < ApplicationController
     def index
         boards = Board.all
-        render json: {status:'SUCCESS', message: 'asdasd' , data: boards}, status: :ok
+        render json: {status:'SUCCESS', message: 'Returning all boards' , data: boards}, status: :ok
     end
 
     def show
         board =Board.find(params[:id])
         squares = Square.references(board)
-        render json: {status:'SUCCESS', message: 'asdasd' , board: board, squares:squares}, status: :ok
+        render json: {status:'SUCCESS', message: 'Returning Specific board' , board: board, squares:squares}, status: :ok
     end
 end
--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -4,4 +4,26 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get 1st board" do
+    @board = boards(:board1)
+
+    @squares = squares.board(@board)
+    get('/boards/1')
+    puts "calling get /boards/1 url"
+    json_response = JSON.parse(response.body)
+
+    board_res = json_response["board"]
+    squares_res = json_response["squares"]
+    puts board_res
+    puts 'response length'
+    puts squares_res.length
+    puts 'board length'
+    puts @squares.length
+
+    # assert_equal(@squares.length, squares_res.length)
+    assert_equal(@board.title, board_res["title"])
+    
+    assert_response :success
+  end
 end
